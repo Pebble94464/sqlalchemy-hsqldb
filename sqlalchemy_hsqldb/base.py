a92e8cedc61028fd30ea11481889c9023252be32
Pebble94464 <jsn-github@pebble.plus.com>
2023-11-03 12:15:02 +0000

    Specify classpath in call to create_engine

--- sqlalchemy_hsqldb/base.py
+++ sqlalchemy_hsqldb/base.py
@@ -7,19 +7,46 @@ from sqlalchemy.engine import reflection
 from sqlalchemy.sql import compiler
 from sqlalchemy.sql import sqltypes
 
+#from sqlalchemy import types, exc, pool
+from sqlalchemy import pool
+
 # Types for mysql, pg and oracle, are defined in their respective types.py file...
 # from .types import DATE
 # from .types import FLOAT
 # from .types import INTERVAL
 
 
+# TODO: Implement HyperSqlCompiler. About 400 lines. The derived subclass is also long.
+class HyperSqlCompiler(compiler.SQLCompiler):
+	pass
+
+# TODO: Implement HyperSqlDDLCompiler. About 100 lines, 7 methods.
+class HyperSqlDDLCompiler(compiler.DDLCompiler):
+	pass
+
+# TODO: Implement HyperSqlTypeCompiler. About 50-150 lines, 12-25 methods.
+# TODO: Solve mystery. Access dialect has 'type_compiler', others have 'type_compiler_cls'
+class HyperSqlTypeCompiler(compiler.GenericTypeCompiler):
+	pass
+
+# TODO: Implement HyperSqlIdentifierPreparer. About 20-55 lines, 2-5 methods.
+class HyperSqlIdentifierPreparer(compiler.IdentifierPreparer):
+	pass
+
+# TODO: Implement HyperSqlExecutionContext. About 55-165 lines, 2-8 methods.
+class HyperSqlExecutionContext(default.DefaultExecutionContext):
+	pass
+
 
 class HyperSqlDialect(default.DefaultDialect):
 	"""HyperSqlDialect implementation of Dialect"""
 
+	def __init__(self, classpath=None, **kwargs):
+		super().__init__(**kwargs)
+		self.classpath = classpath	# A path to the HSQLDB executable jar file.
+
 	# WIP: what are colspecs?
 	#		Descriptions can be found here:  site-packages\sqlalchemy\engine\interfaces.py
-
 
 	# 'colspecs' seems to map sqltypes to types defined in the dialect's types.py file.
 	# default.DefaultDialect.colspecs: MutableMapping[Type[TypeEngine[Any]], Type[TypeEngine[Any]]] = {}
@@ -75,175 +102,42 @@ class HyperSqlDialect(default.DefaultDialect):
 	#
 	# TODO: can be removed / set to True if supported. Access has it set to False.
 
-
-	_need_decimal_fix = False
-
-	supports_is_distinct_from = False
+	#- ok
+	supports_is_distinct_from = True
+	# Supported since HSQLDB v2.0
+	#- Default is True. Access's dialect is set to False.
+	#-
+	#- e.g. SELECT * FROM mytable WHERE col1 IS DISTINCT FROM col2;
+	#- So you can use expressions like col1.is_distinct_from(col2) in SQLAlchemy when using the HSQLDB dialect.
 
 	poolclass = pool.NullPool
+	# QueuePool is the default.
+	# Claude says NullPool or StaticPool is suitable for HSQLDB, but Claude's information might be outdated.
+	# The Access dialect is using NullPool.
+	# poolclass is normally specified as a parameter to create_engine function, e.g.
+	#   create_engine("postgresql+psycopg2://scott:tiger@localhost/test", poolclass=NullPool)
+	# pool_size for QueuePool can is also specified as a create_engine parameter, e.g.
+	#   create_engine("postgresql+psycopg2://me@localhost/mydb", pool_size=20, max_overflow=0)
+	# Detailed info on pooling can be found here:   \sqlalchemy\doc\build\core\pooling.rst
+	#
+	# How is the poolclass assigned to above referenced?
+	#   poolclass is returned by the get_pool_class method of the DefaultDialect.
+	# TODO: Verify if the above is correct.
+	#
+	# TODO: See if QueuePool works, or use StaticPool instead.
+
 	statement_compiler = HyperSqlCompiler
 	ddl_compiler = HyperSqlDDLCompiler
 	type_compiler = HyperSqlTypeCompiler
 	preparer = HyperSqlIdentifierPreparer
 	execution_ctx_cls = HyperSqlExecutionContext
 
-	# TODO: 
 	@DeprecationWarning
 	@classmethod
 	def dbapi(cls):
 		"""
-        A reference to the DBAPI module object itself. (DEPRECATED)
-        It is replaced by import_dbapi, which has been implemented in jaydebeapi.py
-        """
+		A reference to the DBAPI module object itself. (DEPRECATED)
+		It is replaced by import_dbapi, which has been implemented in jaydebeapi.py
+		"""
 		raise NotImplementedError
 
-# TODO: implement HyperSqlCompiler below... (it's currently just a copy of Access's)
-class HyperSqlCompiler(compiler.SQLCompiler):
-    extract_map = compiler.SQLCompiler.extract_map.copy()
-    extract_map.update(
-        {
-            "month": "m",
-            "day": "d",
-            "year": "yyyy",
-            "second": "s",
-            "hour": "h",
-            "doy": "y",
-            "minute": "n",
-            "quarter": "q",
-            "dow": "w",
-            "week": "ww",
-        }
-    )
-
-    def visit_cast(self, cast, **kw):
-        return cast.clause._compiler_dispatch(self, **kw)
-
-    def get_select_precolumns(self, select, **kw):
-        # (plagiarized from mssql/base.py)
-
-        s = super(AccessCompiler, self).get_select_precolumns(select, **kw)
-
-        """ Access puts TOP, it's version of LIMIT here """
-        if select._offset:
-            raise NotImplementedError("Access SQL does not support OFFSET")
-        elif hasattr(select, "_simple_int_limit"):  # SQLA_1.3
-            if select._simple_int_limit:
-                # ODBC drivers and possibly others
-                # don't support bind params in the SELECT clause on SQL Server.
-                # so have to use literal here.
-                s += "TOP %d " % select._limit
-        elif select._has_row_limiting_clause and self._use_top(
-            select
-        ):  # SQLA_1.4
-            # ODBC drivers and possibly others
-            # don't support bind params in the SELECT clause on SQL Server.
-            # so have to use literal here.
-            kw["literal_execute"] = True
-            s += "TOP %s " % self.process(
-                self._get_limit_or_fetch(select), **kw
-            )
-            if select._fetch_clause is not None:
-                if select._fetch_clause_options["percent"]:
-                    s += "PERCENT "
-                if select._fetch_clause_options["with_ties"]:
-                    s += "WITH TIES "
-
-        return s
-
-    def limit_clause(self, select, **kw):
-        """Limit in access is after the select keyword"""
-        return ""
-
-    def binary_operator_string(self, binary):
-        """Access uses "mod" instead of "%" """
-        return binary.operator == "%" and "mod" or binary.operator
-
-    def visit_concat_op_binary(self, binary, operator, **kw):
-        return "%s & %s" % (
-            self.process(binary.left, **kw),
-            self.process(binary.right, **kw),
-        )
-
-    function_rewrites = {
-        "current_date": "now",
-        "current_timestamp": "now",
-        "length": "len",
-    }
-
-    def visit_function(self, func, **kw):
-        """Access function names differ from the ANSI SQL names;
-        rewrite common ones"""
-        func.name = self.function_rewrites.get(func.name, func.name)
-        return super(AccessCompiler, self).visit_function(func)
-
-    def for_update_clause(self, select, **kw):
-        """FOR UPDATE is not supported by Access; silently ignore"""
-        return ""
-
-    # Strip schema
-    def visit_table(self, table, asfrom=False, **kw):
-        if asfrom:
-            return self.preparer.quote(table.name)
-        else:
-            return ""
-
-    def visit_join(self, join, asfrom=False, **kw):
-        return (
-            "("
-            + self.process(join.left, asfrom=True)
-            + (join.isouter and " LEFT OUTER JOIN " or " INNER JOIN ")
-            + self.process(join.right, asfrom=True)
-            + " ON "
-            + self.process(join.onclause)
-            + ")"
-        )
-
-    def visit_extract(self, extract, **kw):
-        field = self.extract_map.get(extract.field, extract.field)
-        return 'DATEPART("%s", %s)' % (field, self.process(extract.expr, **kw))
-
-    def visit_empty_set_expr(self, type_, **kw):
-        literal = None
-        repr_ = repr(type_[0])
-        if repr_.startswith("Integer("):
-            literal = "1"
-        elif repr_.startswith("String("):
-            literal = "'x'"
-        elif repr_.startswith("NullType("):
-            literal = "NULL"
-        else:
-            raise ValueError("Unknown type_: %s" % type(type_[0]))
-        stmt = "SELECT %s FROM USysSQLAlchemyDUAL WHERE 1=0" % literal
-        return stmt
-
-    def visit_ne_binary(self, binary, operator, **kw):
-        return "%s <> %s" % (
-            self.process(binary.left),
-            self.process(binary.right),
-        )
-
-    def _get_limit_or_fetch(self, select):  # SQLA_1.4+
-        if select._fetch_clause is None:
-            return select._limit_clause
-        else:
-            return select._fetch_clause
-
-    def _use_top(self, select):  # SQLA_1.4+
-        return (
-            select._offset_clause is None
-            or (
-                select._simple_int_clause(select._offset_clause)
-                and select._offset == 0
-            )
-        ) and (
-            select._simple_int_clause(select._limit_clause)
-            or (
-                # limit can use TOP with is by itself. fetch only uses TOP
-                # when it needs to because of PERCENT and/or WITH TIES
-                select._simple_int_clause(select._fetch_clause)
-                and (
-                    select._fetch_clause_options["percent"]
-                    or select._fetch_clause_options["with_ties"]
-                )
-            )
-        )
--- sqlalchemy_hsqldb/jaydebeapi.py
+++ sqlalchemy_hsqldb/jaydebeapi.py
@@ -1,11 +1,47 @@
 
 # TODO: Implement the jdbc driver in this file.
+# TODO1: fix "AttributeError("'HyperSqlDialect_jaydebeapi' object has no attribute '_on_connect_isolation_level'")"
 
-from base import HyperSqlDialect
+from .base import HyperSqlDialect
 from types import ModuleType
+from sqlalchemy.engine.url import make_url
 
 class HyperSqlDialect_jaydebeapi(HyperSqlDialect):
 	"""HyperSqlDialect implementation of Dialect using JayDeBeApi as the driver."""
+
+	driver = 'jaydebeapi'
+	jclassname = 'org.hsqldb.jdbc.JDBCDriver'
+
+	# TODO: Clean up the init function below after debugging. Is the method actually required?
+	def __init__(self, **kwargs):
+		HyperSqlDialect.__init__(self,**kwargs)
+
+	def create_connect_args(self, url):
+		""" Returns a tuple consisting of a ``(*args, **kwargs)`` suitable to send directly to the dbapi's connect function. """
+		# Example in parameter 'url' string:	"hsqldb+jaydebeapi://SA:***@localhost:9001/some_database_name"
+		# Example 'jdbc_url' string:			"jdbc:hsqldb:hsql://localhost:9001/some_database_name"
+
+		# TODO: url is expected to be a URL object, so the line immediately below can be removed, possibly.
+		url = make_url(url)
+
+		jdbc_url = f'jdbc:hsqldb:hsql://{url.host}:{url.port}/{url.database}'
+		connectionArgs = {
+			"jclassname": self.jclassname,
+			"url": jdbc_url,
+			"driver_args": [url.username, url.password],
+			"jars" : self.classpath, # HSQLDB executable jar file
+			# "libs" : ""
+		}
+		return ([], connectionArgs)
+
+# jclassname: Full qualified Java class name of the JDBC driver.
+# url: Database url as required by the JDBC driver.
+# driver_args: 	Dictionary or sequence of arguments to be passed to the Java DriverManager.getConnection method.
+# 				Usually sequence of username and password for the db.
+# 				Alternatively a dictionary of connection arguments (where user and password would probably be included).
+# 				See http://docs.oracle.com/javase/7/docs/api/java/sql/DriverManager.html for more details
+# jars: Jar filename or sequence of filenames for the JDBC driver
+# libs: Dll/so filenames or sequence of dlls/sos used as shared library by the JDBC driver
 
 	#- OK
 	@classmethod
