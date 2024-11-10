838439780bdd891665c02692731a7e6bf86121fd
Pebble94464 <jsn-github@pebble.plus.com>
2024-11-10 18:48:31 +0000

    temp 9

--- sqlalchemy_hsqldb/requirements.py
+++ sqlalchemy_hsqldb/requirements.py
@@ -1780,6 +1780,6 @@ class HyperSqlRequirements(SuiteRequirements):
         """Target database reflect MATERIALIZED VIEWs pks."""
         return exclusions.closed()
 
-# TODO: delete this comment 8
+# TODO: delete this comment 9
 
 
